c23f6a0440a44f2b04937ce5e238558210640be7
babenk0ff <babenk0fff@gmail.com>
2022-01-02 14:47:47 +0300

    Added solution for 03, 04

--- 03.py
+++ 03.py
@@ -1,13 +1,13 @@
-def thesaurus(*names):
-    name_letters = set(name[0].lower() for name in names)
+def thesaurus(*names, sort=False):
+    name_letters = set(name[0] for name in names)
     result_dict = {}
     for letter in name_letters:
-        result_dict[letter.upper()] = [name for name in names if name[0].lower() == letter]
+        result_dict[letter] = [name for name in names if name[0] == letter]
+
+    if sort:
+        return dict(sorted(result_dict.items()))
     return result_dict
 
 
-names_dict = thesaurus('Иван', 'Мария', 'Петр', 'Илья', 'Ариэль')
+names_dict = thesaurus('Иван', 'Мария', 'Петр', 'Илья', 'Ариэль', sort=True)
 print(names_dict)
-
-for key in sorted(names_dict.keys()):
-    print(key, names_dict[key])
