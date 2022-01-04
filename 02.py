acb0492479a7e3d87585465902e6ce04a70dcc0d
babenk0ff <babenk0fff@gmail.com>
2022-01-05 00:21:41 +0300

    Added solution

--- 02.py
+++ 02.py
@@ -3,21 +3,30 @@ from decimal import Decimal
 
 
 def currency_rates(currency):
-    url = 'http://www.cbr.ru/scripts/XML_daily.asp'
-    response = requests.get(url)
-    resp_text = response.text
+    if len(currency) == 3:
+        url = 'http://www.cbr.ru/scripts/XML_daily.asp'
+        response = requests.get(url)
+        resp_text = response.text
 
-    if currency.upper() in resp_text:
-        currency_index = resp_text.find(currency.upper())
-        value_start_index = resp_text.find('<Value>', currency_index)
-        value_end_index = resp_text.find('</Value>', currency_index)
-        currency_value = resp_text[value_start_index + 7:value_end_index].replace(',', '.')
-        currency_value = Decimal(currency_value).quantize(Decimal('.01'))
+        if currency.upper() in resp_text:
+            currency_index = resp_text.find(currency.upper())
+            value_start_index = resp_text.find('<Value>', currency_index)
+            value_end_index = resp_text.find('</Value>', currency_index)
+            currency_value = resp_text[value_start_index + 7:value_end_index].replace(',', '.')
+
+            nominal_index_start = resp_text.find('<Nominal>', currency_index)
+            nominal_index_stop = resp_text.find('</Nominal>', currency_index)
+            nominal = int(resp_text[nominal_index_start + 9:nominal_index_stop])
+
+            result = (Decimal(currency_value) / nominal).quantize(Decimal('.01'))
+        else:
+            return None
     else:
         return None
 
-    return currency_value
+    return result
 
 
 print(currency_rates('usd'))
 print(currency_rates('EUR'))
+print(currency_rates('CZK'))
--- 05.py
+++ 05.py
@@ -1,2 +1,16 @@
-from utils import currency_rates
+def main(args):
+    if len(args) > 1:
+        result = currency_rates(args[1])
+        if result:
+            print(*result, sep=', ')
+        else:
+            print(None)
+    else:
+        print(None)
 
+
+if __name__ == '__main__':
+    import sys
+    from utils import currency_rates
+
+    main(sys.argv)
